464332a180ea379cbc55e9f2c420310b056fdea7
ArdaPXL <12401647@student.pxl.be>
2024-11-17 20:03:52 +0100

    MasterMind-03 ComboBoxen De 4 comboboxen worden gevuld met de 6 beschikbare kleuren.

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -46,7 +46,36 @@ namespace Mastermind
                 comboBoxes[i].ItemsSource = colors;
             }
         }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            System.Windows.Controls.Label label = null;
 
-        
+            if (comboBox == ComboBox1) label = Label1;
+            else if (comboBox == ComboBox2) label = Label2;
+            else if (comboBox == ComboBox3) label = Label3;
+            else if (comboBox == ComboBox4) label = Label4;
+
+            if (label != null && comboBox.SelectedItem != null)
+            {
+                string selectedColor = comboBox.SelectedItem.ToString();
+                label.Content = selectedColor;
+                label.Background = GetColorBrush(selectedColor);
+                label.BorderBrush = Brushes.Transparent; // Reset border color
+            }
+        }
+        private Brush GetColorBrush(string colorName)
+        {
+            if (colorName == "Red") return Brushes.Red;
+            if (colorName == "Yellow") return Brushes.Yellow;
+            if (colorName == "Orange") return Brushes.Orange;
+            if (colorName == "White") return Brushes.White;
+            if (colorName == "Green") return Brushes.Green;
+            if (colorName == "Blue") return Brushes.Blue;
+            return Brushes.Transparent;
+        }
+
+
+
     }
 }
