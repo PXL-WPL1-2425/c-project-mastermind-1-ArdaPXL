5261217fba13865094150f077bafa3b9fef1c435
ArdaPXL <12401647@student.pxl.be>
2024-11-17 20:01:43 +0100

    MasterMind-02 Code Er wordt een random code van 4 kleuren gegenereerd. Om het spel makkelijker te testen wordt deze code in de titel van het window getoond. De 4 kleuren worden willekeurig gekozen uit een reeks van 6 beschikbare kleuren: rood, geel, oranje, wit, groen en blauw.

--- mastermind/mastermind/MainWindow.xaml.cs
+++ mastermind/mastermind/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,16 +14,39 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace mastermind
+
+namespace Mastermind
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> colors = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private List<string> code;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeGame();
         }
+
+        private void InitializeGame()
+        {
+            // Generate a random code
+            Random rand = new Random();
+            code = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                code.Add(colors[rand.Next(colors.Count)]);
+            }
+            this.Title = "Mastermind - Code: " + string.Join(", ", code);
+
+            // Populate ComboBoxes with color options
+            ComboBox[] comboBoxes = { ComboBox1, ComboBox2, ComboBox3, ComboBox4 };
+            for (int i = 0; i < comboBoxes.Length; i++)
+            {
+                comboBoxes[i].ItemsSource = colors;
+            }
+        }
+
+        
     }
 }
